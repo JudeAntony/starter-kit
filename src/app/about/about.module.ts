78ee163049b5d74b83c9b7ff8ecdc5f09e0bd04b
Jude Joe Dhanaraj (V) <aruljude_joe@apple.com>
2019-03-10 16:39:29 -0700

    making dashboard header as a component

--- src/app/about/about.module.ts
+++ src/app/about/about.module.ts
@@ -4,9 +4,10 @@ import { TranslateModule } from '@ngx-translate/core';
 
 import { AboutRoutingModule } from './about-routing.module';
 import { AboutComponent } from './about.component';
+import { DashboardHeaderComponent } from './dashboard-header/dashboard-header.component';
 
 @NgModule({
-  imports: [CommonModule, TranslateModule, AboutRoutingModule],
-  declarations: [AboutComponent]
+    imports: [CommonModule, TranslateModule, AboutRoutingModule],
+    declarations: [AboutComponent, DashboardHeaderComponent]
 })
 export class AboutModule {}
