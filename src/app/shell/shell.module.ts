412396884bcf047c311fe772c8d8f150712c5dd5
Jude Joe Dhanaraj (V) <aruljude_joe@apple.com>
2019-04-06 20:44:06 -0700

    added project dropdown

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -8,9 +8,18 @@ import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { BreadcrumbComponent } from './breadcrumb/breadcrumb.component';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [CommonModule, TranslateModule, NgbModule, RouterModule],
+    imports: [
+        CommonModule,
+        FormsModule,
+        TranslateModule,
+        NgbModule,
+        RouterModule,
+        NgSelectModule
+    ],
     declarations: [
         HeaderComponent,
         ShellComponent,
