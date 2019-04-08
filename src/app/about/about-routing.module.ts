ac60e76a6244ba4bbd73014c31e84c36dd39e3a1
Jude Joe Dhanaraj (V) <aruljude_joe@apple.com>
2019-04-07 19:59:40 -0700

    minor fixes

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
         {
             path: 'about',
             component: AboutComponent,
-            data: { title: extract('About') }
+            data: { title: extract('Test-Case') }
         }
     ])
 ];
